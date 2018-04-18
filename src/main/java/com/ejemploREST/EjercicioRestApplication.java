841df9afc49cc3c089f8025889e35d0903b283bb
julio guerrero <julioguerrero1993@gmail.com>
2018-04-18 14:31:41 -0300

    commit inicial

---
+++ src/main/java/com/ejemploREST/EjercicioRestApplication.java
@@ -0,0 +1,12 @@
+package com.ejemploREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioRestApplication.class, args);
+	}
+}
